def841494d15383ecb5a814c92775a8f01bd79b5
Chris Hanma <hongchris96@hotmail.com>
2021-06-12 14:53:26 -0700

    Update user spec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,20 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-
   # Tests: 
   # Error messages for all
 
+  subject(:dummy) { User.new(username: "Bunshin", balance: 100)}
+
   # validations
   describe 'validations' do
-    it "should validate presence of username"
-    it "should validate presence of balance"
-    it "should validate uniqueness of username"
+    it {should validate_presence_of(:username)}
+    it {should validate_presence_of(:balance)}
+    it {should validate_uniqueness_of(:username)}
+    it "should validate balance doesn't go below zero" do
+      dummy.valid?
+      expect(dummy.errors[:balance]).to eq(["Balance cannot go below zero"])
+    end
   end
 
   # associations
   describe 'associations' do
-    it "should have many transactions"
+    it {should have_many(:transactions)}
   end
 
   # class methods
