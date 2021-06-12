d576d13a5d6b24e88555b65901a08e3ecd1cfb08
Chris Hanma <hongchris96@hotmail.com>
2021-06-12 15:26:49 -0700

    Build api routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api, defaults: {format: :json} do
+    resources :users, only: [:index, :show, :create, :update]
+    resources :transactions, only: [:index, :show, :create]
+    resources :spends, only: [:show, :create]
+  end
 end
