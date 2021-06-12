276683d3c0533c9d2b377eda3494451c572e7d5a
Chris Hanma <hongchris96@hotmail.com>
2021-06-12 15:19:46 -0700

    Create Transaction model pass specs

--- app/models/transaction.rb
+++ app/models/transaction.rb
@@ -1,2 +1,8 @@
 class Transaction < ApplicationRecord
+  validates :payer_id, :points, :created_at, presence: true
+  
+  belongs_to :payer,
+    foreign_key: :payer_id,
+    class_name: :User
+  
 end
