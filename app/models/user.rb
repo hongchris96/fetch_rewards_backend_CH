cf6c7e5c855b4d425995b14faf986b963ae1c2c4
Chris Hanma <hongchris96@hotmail.com>
2021-06-12 15:07:06 -0700

    Create User model, config shoulda matchers, 1 spec failed

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,10 @@
 class User < ApplicationRecord
+  validates :username, presence: true, uniqueness: true
+  validates :balance, presence: true, numericality: {greater_than_or_equal_to: 0}
+
+  has_many :transactions,
+    foreign_key: :payer_id,
+    class_name: :Transaction
+
+  
 end
