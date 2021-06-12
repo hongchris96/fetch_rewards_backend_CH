f5d61cc351c9bd7f0dd0fe94f9d5fa8d90abe4ba
Chris Hanma <hongchris96@hotmail.com>
2021-06-12 15:58:08 -0700

    Update routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   namespace :api, defaults: {format: :json} do
-    resources :users, only: [:index, :show, :create, :update]
-    resources :transactions, only: [:index, :show, :create]
-    resources :spends, only: [:show, :create]
+    resources :users, only: [:index, :show, :create, :update, :new]
+    resources :transactions, only: [:index, :show, :create, :new]
+    resources :spends, only: [:show, :create, :new]
   end
 end
