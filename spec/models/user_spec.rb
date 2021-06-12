09b44a54f9204c8bee270161cf0b21cd31310976
Chris Hanma <hongchris96@hotmail.com>
2021-06-12 14:42:10 -0700

    Write specs for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  # Tests: 
+  # Error messages for all
+
+  # validations
+  describe 'validations' do
+    it "should validate presence of username"
+    it "should validate presence of balance"
+    it "should validate uniqueness of username"
+  end
+
+  # associations
+  describe 'associations' do
+    it "should have many transactions"
+  end
+
+  # class methods
+  # describe 'class methods' do
+    
+  # end
+
 end
