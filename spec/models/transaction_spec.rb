e22eb6492111964270974e14ae5b586f8a8b2a84
Chris Hanma <hongchris96@hotmail.com>
2021-06-12 15:17:21 -0700

    Create transaction model spec

--- spec/models/transaction_spec.rb
+++ spec/models/transaction_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe Transaction, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  # Tests: 
+  # Error messages for all
+
+  # validations
+  describe 'validations' do
+    it {should validate_presence_of(:payer_id)}
+    it {should validate_presence_of(:points)}
+    it {should validate_presence_of(:created_at)}
+  end
+
+  # associations
+  describe 'associations' do
+    it { should belong_to(:payer) }
+  end
+
+
 end
