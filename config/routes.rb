5a801922adaa6deaca2eb7e63279817172f84f4f
Chris Hanma <hongchris96@hotmail.com>
2021-06-12 17:30:23 -0700

    Update user controller, remove api namespace

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  namespace :api, defaults: {format: :json} do
-    resources :users, only: [:index, :show, :create, :update, :new]
-    resources :transactions, only: [:index, :show, :create, :new]
-    resources :spends, only: [:show, :create, :new]
-  end
+  root 'users#new'
+
+  resources :users, only: [:index, :show, :create, :update, :new]
+  resources :transactions, only: [:index, :show, :create, :new]
+  resources :spends, only: [:show, :create, :new]
 end
